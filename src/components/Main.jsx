5f988d4382af7e6480c7290249b0c148cea2a555
Alanmad06 <madrigal.saenz.alan@gmail.com>
2024-06-06 19:45:30 -0600

    Change title Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ export function Main(){
             <div className="main__logo">LOGO</div>
             <div className="main__text">
                 <h1 className="main__text-h1">
-                    HEADLINE <br/> here
+                    VM <br/> CRAFT
                 </h1>
                 <p className="main__text-p">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.Debitis facere doloremque
