d81829c6f244d5a0ec43310152c8eeed37190407
Alanmad06 <madrigal.saenz.alan@gmail.com>
2024-07-03 19:00:55 -0600

    Update Subscribe component Test

--- src/tests/components-tests/Subscribe.test.js
+++ src/tests/components-tests/Subscribe.test.js
@@ -1,14 +1,19 @@
+// src/tests/components-tests/Subscribe.test.js
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { Subscribe } from '../../components/Subscribe';
+import { Provider } from 'react-redux';
+import {store} from '../../redux/store';
+import {Subscribe} from '../../components/Subscribe';
 
-describe("<Subscribe/>",()=>{
-
-    test("Renderizado componente Subscribe" ,()=>{
-        render(<Subscribe/>)
-        const titleElement = screen.getByText(/Join Our Program/i)
-        expect(titleElement).toBeInTheDocument()
-    })
-
-
-})
+describe('<Subscribe/>', () => {
+  test('Renderizado componente Subscribe', () => {
+    render(
+      <Provider store={store}>
+        <Subscribe />
+      </Provider>
+    );
+    const titleElement = screen.getByText(/Join Our Program/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+});
 
